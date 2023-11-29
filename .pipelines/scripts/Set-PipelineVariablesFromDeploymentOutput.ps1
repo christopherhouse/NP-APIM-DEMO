ea107739b412c230f40482ca2e5f9b15d5064a14
Christopher House <chhouse@microsoft.com>
2023-11-29 14:31:55 -0600

    initial add of pipelines and related resources (#2)

---
+++ .pipelines/scripts/Set-PipelineVariablesFromDeploymentOutput.ps1
@@ -0,0 +1,19 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $armOutputString
+)
+
+Write-Output "Deployment output is $armOutputString"
+$outputObj = $armOutputString | ConvertFrom-Json
+$functionAppName = $outputObj.functionAppName.value
+$keyVaultName = $outputObj.keyVaultName.value
+
+# Set variable with Function app name from deployment output.  This will be used
+# as input for the Function deploy step
+Write-Output "##vso[task.setvariable variable=functionAppName;]$functionAppName"
+
+# Set variable for Key Vault name.  This is used in a script to update a kv secret
+Write-Output "##vso[task.setvariable variable=keyVaultName;]$keyVaultName"
