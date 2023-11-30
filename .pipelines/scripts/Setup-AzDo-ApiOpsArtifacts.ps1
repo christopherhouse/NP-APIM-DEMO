a216730c1ce9ebaf0b2ed9371505e1feceb3815c
Chris House <chhouse@microsoft.com>
2023-11-30 10:23:46 -0600

    updated script to create pls

--- .pipelines/scripts/Setup-AzDo-ApiOpsArtifacts.ps1
+++ .pipelines/scripts/Setup-AzDo-ApiOpsArtifacts.ps1
@@ -3,6 +3,11 @@ param(
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty]
     [string]
+    $PrefixString,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty]
+    [string]
     $AzDoOrgName,
 
     [Parameter(Mandatory)]
@@ -15,4 +20,37 @@ param(
     [string]
     $AzDoRepositoryName
 )
+
+$buildAndDeployPipelinePath = ".\.pipelines\build-and-deploy-order-fulfillment.yaml"
+$apiOpsExtractorPipelinePath = ".\.pipelines\run-extractor.yaml"
+$apiOpsPublisherPipelinePath = ".\.pipelines\run-publisher.yaml"
+$buildAndDeployPipelineName = "${PrefixString}-ApiOps-BuildAndDeploy"
+$extractPipelineName = "${PrefixString}-ApiOps-Extractor"
+$publisherPipelineName = "${PrefixString}-ApiOps-Publisher"
+
+# Use the command 'az pipelines create' to create a new YAML pipeline
+# in the DevOps org and project specified by the $AzDoOrgName and $AzDoProjectName
+# parameters.  The pipeline will be created in the repository specified by the
+# $AzDoRepositoryName parameter.  The pipeline will be created from the YAML
+# file specified by the $buildAndDeployPipelinePath parameter.  The pipeline
+# name will be specified by the $buildAndDeployPipelineName parameter.
+Write-Output "Creating pipeline $buildAndDeployPipelineName from $buildAndDeployPipelinePath"
+az pipelines create --name $buildAndDeployPipelineName `
+                    --org $AzDoOrgName `
+                    --project $AzDoProjectName `
+                    --repository $AzDoRepositoryName `
+                    --repository-type tfsgit `
+                    --branch master `
+                    --skip-first-run `
+                    --yaml-path $buildAndDeployPipelinePath
+
+Write-Output "Creating pipeline $buildAndDeployPipelineName from $buildAndDeployPipelinePath"
+az pipelines create --name $buildAndDeployPipelineName `
+                    --org $AzDoOrgName `
+                    --project $AzDoProjectName `
+                    --repository $AzDoRepositoryName `
+                    --repository-type tfsgit `
+                    --branch master `
+                    --skip-first-run `
+                    --yaml-path $buildAndDeployPipelinePath
 
